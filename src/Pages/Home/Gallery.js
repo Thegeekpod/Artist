917ed640c356bf6c2f02faefe61b4f3fd703979d
Thegeekpod <supriyo7dey@gmail.com>
2023-11-26 04:45:57 +0530

    m

--- src/Pages/Home/Gallery.js
+++ src/Pages/Home/Gallery.js
@@ -3,50 +3,65 @@ import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
 
 export default function MGallery({ image }) {
-  const [open, setOpen] = useState(false);
-  const [selectedImageIndex, setSelectedImageIndex] = useState(0); // Initializing with 0 as default index
+  const [selectedImageIndex, setSelectedImageIndex] = useState(null);
 
   const handleImageClick = (index) => {
     setSelectedImageIndex(index);
-    setOpen(true);
+  };
+
+  const closeLightbox = () => {
+    setSelectedImageIndex(null);
   };
 
   return (
     <>
-       {image.map((item,index) => (
-           <div className="col-lg-4 " key={item.id}>
-           <div class="imgbox">
-           <img className="imgbo" src={`https://sweetdevelopers.com/artist/storage/ArtworkImage/${item.image}`} alt={item.title}  />
- <div className='imgtitle'>
-             <h4 class="page-title-1 " style={{color:'white'}}>
-              {item.user?.username || item.title}
-             </h4>
-             <div className="row d-flex">
-               
-               <div className="coll-6 text-left">
-               
-               <i class="fa fa-thumbs-up like" aria-hidden="true"> 1</i>
-                   
-                
-
-               </div>
-               <div className="coll-6 text-right">
-               <i class="fa fa-eye like" aria-hidden="true" onClick={() => handleImageClick(index)}> 1</i>
-               </div>
-             </div>
-             </div>
-           
-           </div>
-           </div>
-          ))}
-      <Lightbox
-        selectedIndex={selectedImageIndex}
-        open={open}
-        close={() => setOpen(false)}
-        slides={image.map((item) => ({
-          src: `https://sweetdevelopers.com/artist/storage/ArtworkImage/${item.image}`,
-        }))}
-      />
+      {image.map((item, index) => (
+        <div className="col-lg-4" key={item.id}>
+          <div className="imgbox">
+            <img
+              className="imgbo"
+              src={`https://sweetdevelopers.com/artist/storage/ArtworkImage/${item.image}`}
+              alt={item.title}
+              
+            />
+            <div className="imgtitle">
+              <h4 className="page-title-1" style={{ color: "white" }}>
+                {item.user?.username || item.title}
+              </h4>
+              <div className="row d-flex">
+                <div className="coll-6 text-left">
+                  <i className="fa fa-thumbs-up like" aria-hidden="true">
+                    {" "}
+                    1
+                  </i>
+                </div>
+                <div className="coll-6 text-right">
+                  <i
+                    className="fa fa-eye like"
+                    aria-hidden="true"
+                    onClick={() => handleImageClick(index)}
+                  >
+                    {" "}
+                    1
+                  </i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+      {selectedImageIndex !== null && (
+        <Lightbox
+          selectedIndex={selectedImageIndex}
+          open={true}
+          close={closeLightbox}
+          slides={[
+            {
+              src: `https://sweetdevelopers.com/artist/storage/ArtworkImage/${image[selectedImageIndex].image}`,
+            },
+          ]}
+        />
+      )}
     </>
   );
 }
