50ba781358b88ebdca6161ed75651003f247abbf
Thegeekpod <supriyo7dey@gmail.com>
2023-11-28 02:46:13 +0530

    j

--- src/Pages/Account/Register.js
+++ src/Pages/Account/Register.js
@@ -5,117 +5,56 @@ import { isAuthenticated, useUser } from './UserContext';
 import { apibaseUrl } from '../../Component/Apibaseurl';
 
 const Register = () => {
-    const [username, setUsername] = useState('');
-    const [name, setName] = useState('');
+  const [username, setUsername] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState({}); // State to hold field-specific errors
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState(false);
-    const [sucess, setSucess] = useState(false);
+  // ... (existing code)
 
-   const navigate=  useNavigate();
-   useEffect(() => {
-    // Check if the user is authenticated when the component mounts
-    if (isAuthenticated()) {
-      // Redirect to the login page if not authenticated
-      navigate('/profile');
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []); 
   const handleFormSubmit = async (e) => {
     e.preventDefault();
-  
+
     try {
-      // const proxyUrl = 'https://sweetdevelopers.com/proxy.php?url=https://sweetdevelopers.com/artist/api/register';
-  
       const response = await axios.post(`${apibaseUrl}/register`, {
         name,
         username,
         email,
         password,
       });
-  
+
       const data = response.data;
-      if (data.status === false) {
-        // Handle incorrect ID or password scenario
-        console.error('Incorrect ID or password');
-        setError(true);
-        setTimeout(() => {
-          setError(true);
-        }, 5000);
-        // You might want to update state, show an error message, etc.
+      if (data.errors) {
+        // Update state with errors if present in the response
+        setErrors(data.errors);
       } else if (data.status === true) {
         setSucess(true);
         setTimeout(() => {
-            navigate('/login');
-          }, 5000);
+          navigate('/login');
+        }, 5000);
         console.log(data.data);
-       
-        
-      } else {
-        // Handle other scenarios if needed
       }
-  
-  console.log(data)
 
-     
+      console.log(data);
     } catch (error) {
       console.error('Error submitting form:', error);
       // Handle errors appropriately, update state, show error messages, etc.
     }
   };
-  
-      
+
   return (
     <>
-    <div className="page_breadcrumbs breadcrumbs-type-1">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-12"></div>
-        </div>
-        <div className="row">
-          <div className="col-lg-4 col-md-4 col-sm-4 text-left text-sm-center">
-            <ul className="breadcrumbs-list">
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li className="active">Register</li>
-            </ul>
-          </div>
-          <div className="col-lg-4 col-md-4 col-sm-4 text-center">
-            <h3 className="breadcrumbs-title">Register Now</h3>
-          </div>
-          <div className="col-lg-4 col-md-4 col-sm-4" />
-        </div>
-      </div>
-    </div>
-    <div className="ds section_padding_top_100 section_padding_bottom_100">
-      <div className="container">
-        <div className="row">
-        <form className="shop-register" onSubmit={handleFormSubmit}>
-      <div className="col-sm-12">
-        {error && <p className='error'>Criteria does not match</p>}
-        {sucess && <p className='sucess'>Registration Sucessfull Please login Now</p>}
-        <div className="form-group validate-required validate-email" id="billing_username_field">
-          <label htmlFor="billing_username" className="control-label">
-            <span className="grey">Name </span>
-            <span className="required">*</span>
-          </label>
-          <input required
-            type="text"
-            className="form-control"
-            name="billing_name"
-            id="billing_name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
+      {/* ... (existing code) */}
+      <form className="shop-register" onSubmit={handleFormSubmit}>
+        {/* ... (existing code) */}
         <div className="form-group validate-required validate-email" id="billing_username_field">
           <label htmlFor="billing_username" className="control-label">
             <span className="grey">Username </span>
             <span className="required">*</span>
           </label>
-          <input required
+          <input
+            required
             type="text"
             className="form-control"
             name="billing_username"
@@ -123,6 +62,7 @@ const Register = () => {
             value={username}
             onChange={(e) => setUsername(e.target.value)}
           />
+          {errors.username && <p className='error'>{errors.username[0]}</p>}
         </div>
 
         <div className="form-group validate-required validate-email" id="billing_email_field">
@@ -130,7 +70,8 @@ const Register = () => {
             <span className="grey">Email Address </span>
             <span className="required">*</span>
           </label>
-          <input required
+          <input
+            required
             type="text"
             className="form-control"
             name="billing_email"
@@ -138,6 +79,7 @@ const Register = () => {
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
+          {errors.email && <p className='error'>{errors.email[0]}</p>}
         </div>
 
         <div className="form-group" id="billing_password_field">
@@ -145,7 +87,8 @@ const Register = () => {
             <span className="grey">Password</span>
             <span className="required">*</span>
           </label>
-          <input required
+          <input
+            required
             type="password"
             className="form-control"
             name="billing_password"
@@ -153,21 +96,14 @@ const Register = () => {
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
+          {errors.password && <p className='error'>{errors.password[0]}</p>}
         </div>
-      </div>
+        {/* ... (existing code) */}
+      </form>
+      {/* ... (existing code) */}
+    </>
+  );
+};
 
-      <div className="col-sm-12">
-        <button type="submit" className="theme-button">
-          Register
-        </button>
-      </div>
-    </form>
-        </div>
-      </div>
-    </div>
-  </>
-  
-  )
-}
 
 export default Register
