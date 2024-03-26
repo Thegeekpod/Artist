01f65cad9918308823736dee0212ce6e029c2566
Thegeekpod <supriyo7dey@gmail.com>
2024-03-26 21:10:05 +0530

    g

--- src/Component/MapContainer.js
+++ src/Component/MapContainer.js
@@ -1,22 +1,22 @@
 import React, { useRef, useEffect } from 'react';
 import { GoogleApiWrapper, Map, Marker } from 'google-maps-react';
 
-const MapContainer = ({ google, lat, lng }) => {
+const MapContainer = ({ google, latitude, longitude }) => {
   const mapRef = useRef(null);
 
   useEffect(() => {
     if (!google || !mapRef.current) return;
 
     const map = new google.maps.Map(mapRef.current, {
-      center: { lat: lat , lng: lng },
+      center: { latitude: latitude , longitude: longitude },
       zoom: 14
     });
 
     new google.maps.Marker({
-      position: { lat: lat , lng: lng },
+      position: { latitude: latitude , longitude: longitude },
       map: map
     });
-  }, [google, lat, lng]);
+  }, [google, latitude, longitude]);
 
   return (
     <div style={{ width: '100%', height: '430px' }}>
